79239c8d07e8281a0d356e26070ba34f6134a2a2
Sagi Haklay <sahaklay@gmail.com>
2025-07-15 12:41:11 +0300

    fixed modal animations and added form validation

--- main.js
+++ main.js
@@ -3,3 +3,60 @@ document.getElementById('modal-exit-btn').addEventListener('click', () => {
     modalContainer.classList.remove('modal-container-popup');
     modalContainer.classList.add('modal-container-exit');
 });
+setTimeout(() => {
+    modalContainer.classList.add('modal-container-popup');
+}, 1000);
+
+function validateForm(submitBtnId, nameInputId, phoneInputId, emailInputId) {
+    const submitBtn = document.getElementById(submitBtnId);
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const nameInput = document.getElementById(nameInputId);
+        const phoneInput = document.getElementById(phoneInputId);
+        const emailInput = document.getElementById(emailInputId);
+        let valid = true;
+        if (nameInput.value.length === 0) {
+            nameInput.classList.add('invalid');
+            nameInput.placeholder = "*נא להזין שם תקין";
+            valid = false;
+        }
+        const validPhoneChars = '0123456789-';
+        if (phoneInput.value.length === 0 || 
+            ![...phoneInput.value].every((char) => validPhoneChars.includes(char))) {
+            phoneInput.classList.add('invalid');
+            phoneInput.placeholder = "*נא להזין מספר טלפון";
+            phoneInput.value = "";
+            valid = false;
+        }
+        if (emailInput.value.length === 0) {
+            emailInput.classList.add('invalid');
+            emailInput.placeholder = "*נא להזין כתובת מייל";
+            valid = false;
+        }
+        if (valid) {
+            if (nameInput.classList.contains('invalid')) {
+                nameInput.classList.remove('invalid');
+                nameInput.placeholder = "שם מלא";
+            }
+            if (phoneInput.classList.contains('invalid')) {
+                phoneInput.classList.remove('invalid');
+                phoneInput.placeholder = "מספר טלפון";
+            }
+            if (emailInput.classList.contains('invalid')) {
+                emailInput.classList.remove('invalid');
+                emailInput.placeholder = "אי-מייל";
+            }
+            nameInput.value = "";
+            phoneInput.value = "";
+            emailInput.value = "";
+        }
+    });
+}
+
+validateForm("modal-submit-btn", "modal-name-input", "modal-phone-input", "modal-email-input");
+validateForm("contact-submit-btn", "contact-fullname-input", "contact-phone-input", "contact-email-input");
+validateForm("footer-submit-btn", "footer-fullname-input", "footer-phone-input", "footer-email-input");
+document.getElementById('modal-submit-btn').addEventListener('click', () => {
+    modalContainer.classList.remove('modal-container-popup');
+    modalContainer.classList.add('modal-container-exit');
+});
